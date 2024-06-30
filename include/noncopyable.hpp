c0ee3249f67ca31d6c961719c8a4495c3cc9a4fc
Krewoe <2192629378@qq.com>
2024-06-30 15:08:38 +0800

    不可拷贝类

---
+++ include/noncopyable.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file noncopyable.hpp
+ * @author qc
+ * @brief
+ * @version 0.1
+ * @date 2024-06-30
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+namespace qc {
+
+/// @brief 不可拷贝类型
+class Noncopyable {
+public:
+    Noncopyable() = default;
+
+    ~Noncopyable() = default;
+
+    Noncopyable(const Noncopyable&) = delete;
+
+    Noncopyable& operator=(const Noncopyable&) = delete;
+    
+};
+
+}  // namespace qc
