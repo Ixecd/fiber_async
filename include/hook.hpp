ad72770d7e90d7167819e72a24a2b599702bf48d
Krewoe <2192629378@qq.com>
2024-07-06 20:49:56 +0800

    hook

---
+++ include/hook.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file hook.hpp
+ * @author qc
+ * @brief Hook
+ * @version 0.1
+ * @date 2024-07-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#pragma once 
+
+namespace qc {
+
+
+
+}
